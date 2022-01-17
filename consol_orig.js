83c8e2c11135d37a71871094317dcf6f94d4948b
MemorXuxu <97904901+MemorXuxu@users.noreply.github.com>
2022-01-18 01:04:59 +0800

    Update consol_orig.js

--- consol_orig.js
+++ consol_orig.js
@@ -1,7 +1,7 @@
 //评教页面前面的内容
 labeliddd =document.getElementsByClassName("webix_inp_label");
 nameofbookid = labeliddd[3].htmlFor;
-document.getElementById(nameofbookid).value="2021年版的《中国马克思主义与当代》";
+document.getElementById(nameofbookid).value="我是一本书";
 classlanguageid = labeliddd[4].htmlFor;
 document.getElementById(classlanguageid).value="中文";
 tmpselction =document.getElementsByClassName("webix_inp_radio_border webix_radio_0");
@@ -29,10 +29,10 @@ box2id = box[12].firstElementChild.id;
 box3id = box[13].firstElementChild.id;
 box4id = box[15].firstElementChild.id;
 sumbitid = box[16].firstElementChild.id;   //提交按钮 慎用！！！
-document.getElementById(box1id).value='老师上课富有激情，语言风趣幽默，具有启发性';
-document.getElementById(box2id).value='没有';
-document.getElementById(box3id).value='没有';
-document.getElementById(box4id).value='课程形式很好，内容丰富，紧跟时事';
+document.getElementById(box1id).value='XXXXX';
+document.getElementById(box2id).value='XXXXX';
+document.getElementById(box3id).value='XXXXX';
+document.getElementById(box4id).value='XXXXX';
 
 //试验按钮，不建议用
 document.getElementById(box4id).click(); //自动保存并提交，危险性比较大，不建议加到脚本里面，别这么
